f864ae49b620e87ee913001a5703ae6771f9f52a
jeenakarmi <jinanakarmi@gmail.com>
2024-02-19 19:09:53 +0545

    Add local storage

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 const notes = document.querySelectorAll(".input-box")     //selects all notes
 
+
+//showing notes if in localstorage
+function showNotes() 
+{
+    notesContainer.innerHTML = localStorage.getItem("notes"); 
+}
+
+// Retrieve notes from local storage when the page loads
+showNotes();
+
+function updateStorage()
+{
+    // all written in notesContainer will store in browser with name notes
+    localStorage.setItem("notes",notesContainer.innerHTML);
+}
+
 createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -18,6 +34,12 @@ createBtn.addEventListener("click", () => {
         if (event.key === "Backspace" && isCaretAtStart(inputBox)) {
             event.preventDefault();  //prevent the default action of the "Backspace" key
         }
+    });
+
+    // Event listener for creating new note
+    img.addEventListener("click", () => {
+        inputBox.remove();
+        updateStorage();
     });
 
     notesContainer.appendChild(inputBox).appendChild(img);
@@ -38,5 +60,22 @@ notesContainer.addEventListener("click",function(e)  //e as event object
     if(e.target.tagName ==="IMG")
     {
         e.target.parentElement.remove()
+        updateStorage();  //updating after deletion
+    } 
+    //when write in p element it will update
+    else if(e.target.tagName === "P") 
+    {
+        notes = document.querySelectorAll(".input-box");
+        //update data for all notes
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
     }
 })
+
+// Event listener for detecting changes in notes
+notesContainer.addEventListener("input", () => {
+    updateStorage();
+});
